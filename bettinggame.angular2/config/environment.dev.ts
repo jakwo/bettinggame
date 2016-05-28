88e25198ecba04416687a44e52dbe0eb96f64239
jakwo <jakob.wolf85@gmail.com>
2016-05-28 13:56:08 +0200

    Changed dev config.

--- bettinggame.angular2/config/environment.dev.ts
+++ bettinggame.angular2/config/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,  
-  auth0client: 'unvJYUvFAuYWd1hSXFXRENZwXphg17Lm',
+  auth0client: 'o89zWOnbe8eOAXgnQaHDuVShQ56BMrFf',
   auth0url: 'testa.eu.auth0.com',
-  api:'http://localhost:43822/api'
+  api:'http://bettinggameapi.azurewebsites.net/api'
 };
