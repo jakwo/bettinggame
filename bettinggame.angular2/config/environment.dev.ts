ac94948a580ae0d7fa86f081f36dd84ad076eae2
jakwo <jakob.wolf85@gmail.com>
2016-05-28 14:02:05 +0200

    Revert "Changed dev config."
    This reverts commit 88e25198ecba04416687a44e52dbe0eb96f64239.

--- bettinggame.angular2/config/environment.dev.ts
+++ bettinggame.angular2/config/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,  
   auth0client: 'o89zWOnbe8eOAXgnQaHDuVShQ56BMrFf',
   auth0url: 'testa.eu.auth0.com',
-  api:'http://bettinggameapi.azurewebsites.net/api'
+  api:'http://localhost:43822/api'
 };
