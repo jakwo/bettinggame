d1dece1fd88f2193e1c146ee214459420b2c7cc7
jakwo <jakob.wolf85@gmail.com>
2016-06-05 00:25:13 +0200

    Update environment.prod.ts

--- bettinggame.angular2/config/environment.prod.ts
+++ bettinggame.angular2/config/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  // auth0client: 'x8d4iM8HCdDOOFLyMM98j5WyOc4U7p9U',
-  // auth0url: 'testa.eu.auth0.com',
-  // api:'http://em16tippsapi.azurewebsites.net/api'
   auth0client: 'unvJYUvFAuYWd1hSXFXRENZwXphg17Lm',
   auth0url: 'testa.eu.auth0.com',
   api: 'http://localhost:43822/api'
